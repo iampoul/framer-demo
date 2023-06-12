e9f4b0c28bd9117c66375eb3391a2679e4369143
Poul Poulsen <hello@poul.dev>
2023-06-12 15:17:59 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Logo from "@/app/components/Logo";
-import Products from "@/app/components/Products";
+import Logo from '@/app/components/Logo'
+import Products from '@/app/components/Products'
 
 const Home = () => {
-
-
     return (
         <main className="flex flex-col items-center p-24 place-items-start">
             <Logo />
